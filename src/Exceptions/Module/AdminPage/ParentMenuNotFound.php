537b1be3c71a321a5a7d765986792b385f12a8a2
Christoph Friedrich <christoph.friedrich@vonaffenfels.de>
2022-08-29 21:52:18 +0200

    Refactored the way menu items get registered. Reason is to save memory while not in admin backend (in the old way the menu objects where created regardless of their need)

---
+++ src/Exceptions/Module/AdminPage/ParentMenuNotFound.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace VAF\WP\Library\Exceptions\Module\AdminPage;
+
+use Exception;
+use Throwable;
+use VAF\WP\Library\AbstractPlugin;
+
+final class ParentMenuNotFound extends Exception
+{
+    final public function __construct(AbstractPlugin $plugin, string $parentSlug, Throwable $previous = null)
+    {
+        $message = sprintf(
+            '[Plugin %s] Parent menu with slug "%s" could not be found!',
+            $plugin->getPluginSlug(),
+            $parentSlug
+        );
+        parent::__construct($message, 0, $previous);
+    }
+}
