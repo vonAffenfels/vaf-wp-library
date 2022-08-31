badd3515a7499d8166623dba62964c37c23dd22d
Christoph Friedrich <christoph.friedrich@vonaffenfels.de>
2022-08-31 16:00:29 +0200

    Refactored AdminPages Module

--- src/Modules/AdminPagesModule.php
+++ src/Modules/AdminPagesModule.php
@@ -5,6 +5,7 @@ namespace VAF\WP\Library\Modules;
 use Closure;
 use VAF\WP\Library\AdminPages\AdminPage;
 use VAF\WP\Library\Exceptions\Module\AdminPage\InvalidAdminPageClass;
+use VAF\WP\Library\Exceptions\Module\AdminPage\ParentMenuNotFound;
 
 final class AdminPagesModule extends AbstractHookModule
 {
@@ -12,6 +13,13 @@ final class AdminPagesModule extends AbstractHookModule
      * @var array List of all adminpage classes
      */
     private array $adminPages = [];
+
+    private static array $adminPageSlugMapper = [];
+
+    /**
+     * @var AdminPage[]
+     */
+    private array $laterChildMenus = [];
 
     /**
      * Returns a callable that is run to configure the module
@@ -41,7 +49,123 @@ final class AdminPagesModule extends AbstractHookModule
     {
         return [
             'admin_menu' => function () {
+                $this->loadPages();
             }
         ];
+    }
+
+    /**
+     * @return void
+     * @throws ParentMenuNotFound
+     */
+    final private function loadPages(): void
+    {
+        foreach ($this->adminPages as $class) {
+            $adminPageObj = new $class($this->getPlugin());
+
+            $this->registerMenuItem($adminPageObj);
+        }
+
+        foreach ($this->laterChildMenus as $childMenu) {
+            if (!$this->registerChildMenuItem($childMenu)) {
+                throw new ParentMenuNotFound($this->getPlugin(), $childMenu->getMenu()->getParent());
+            }
+        }
+    }
+
+    /**
+     * @param  AdminPage $page
+     * @return void
+     */
+    final private function registerMenuItem(AdminPage $page): void
+    {
+        $menuItem = $page->getMenu();
+        $menuItem->lockObject();
+        $parent = $menuItem->getParent();
+
+        if (is_null($parent)) {
+            // We are registering a main menu item
+            $slugPrefix = $this->getPlugin()->getPluginSlug() . '-';
+
+            add_menu_page(
+                $page->getTitle(),
+                $menuItem->getMenuTitle(),
+                'manage_options',
+                $slugPrefix . $menuItem->getSlug(),
+                function () use ($page) {
+                    echo sprintf('Page %s - Menu Item %s', $page->getTitle(), $page->getMenu());
+                },
+                $menuItem->getIcon(),
+                $menuItem->getPosition()
+            );
+
+            // Add atleast one child to give the option to name the submenu item differently
+            // when having children
+            add_submenu_page(
+                $slugPrefix . $menuItem->getSlug(),
+                $page->getTitle(),
+                $menuItem->getSubMenuTitle(),
+                'manage_options',
+                $slugPrefix . $menuItem->getSlug(),
+                '__return_false',
+                null
+            );
+
+            self::$adminPageSlugMapper[get_class($page)] = $slugPrefix . $menuItem->getSlug();
+        } else {
+            // We will add a child
+            // First we will check if parent exists
+            if (!$this->registerChildMenuItem($page)) {
+                // Menu item could not be added at this time
+                // So we save it for it might be available later
+                $this->laterChildMenus[] = $page;
+            }
+        }
+    }
+
+    final private function registerChildMenuItem(AdminPage $page): bool
+    {
+        $menuItem = $page->getMenu();
+        $parent = $menuItem->getParent();
+        $slugPrefix = $this->getPlugin()->getPluginSlug() . '-';
+
+        if (isset(self::$adminPageSlugMapper[$parent])) {
+            $parent = self::$adminPageSlugMapper[$parent];
+        }
+
+        if (!$this->hasParentMenu($parent)) {
+            return false;
+        }
+
+        add_submenu_page(
+            $parent,
+            $page->getTitle(),
+            $menuItem->getMenuTitle(),
+            'manage_options',
+            $slugPrefix . $menuItem->getSlug(),
+            function () use ($page) {
+                echo sprintf('Page %s - Menu Item %s', $page->getTitle(), $page->getMenu());
+            },
+            $menuItem->getPosition()
+        );
+
+        return true;
+    }
+
+    /**
+     * Function to check if the slug provided is already registered as a top level menu item
+     *
+     * @param string $slug
+     * @return bool
+     */
+    final private function hasParentMenu(string $slug): bool
+    {
+        global $menu;
+
+        $parentMenuArr = array_filter($menu, function (array $menuItem) use ($slug): bool {
+            return $menuItem[2] == $slug;
+        });
+
+        return !empty($parentMenuArr);
     }
 }
